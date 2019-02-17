091d36b25ee8d9194b0880c7d6b99ad6a1773d21
Joshua Dennis <jdenn11@u.rochester.edu>
2019-02-17 13:09:14 -0600

    More stuff

--- yiqun.php
+++ yiqun.php
@@ -17,23 +17,22 @@
 </aside>
 
 <main>
-	<h2>Background</h2>
-	<p> 
-	Hi everyone! My Chinese name is Yiqun Ding but I go by Juliet. I am from Shanghai, China; however, this is already my sixth year in States. I went to a private high school called Salem Academy in North Carolina. The school was in a really small city called Winston-Salem, where Wake Forest University is, so I decided to go to a larger city where there are more resources and I can do more things. I am currently a sophomore at University of Rochester. I am trying to double major in Digital Media Studies and Psychology. </p>
+	<section class="design">
+		<article class="item">
+			<h2>Background</h2>
+			<p>Hi everyone! My Chinese name is Yiqun Ding but I go by Juliet. I am from Shanghai, China; however, this is already my sixth year in States. I went to a private high school called Salem Academy in North Carolina. The school was in a really small city called Winston-Salem, where Wake Forest University is, so I decided to go to a larger city where there are more resources and I can do more things. I am currently a sophomore at University of Rochester. I am trying to double major in Digital Media Studies and Psychology. </p>
+		</article>
 
-	<h2>Before College</h2>
-	<p>
-	I moved to the States six years ago, moving to North Carolina. I went to a private high school called Salem Academy. The school was in a really small city called Winston-Salem, where Wake Forest University is, so I decided to go to a larger city where there are more resources and I can do more things. During high school, I found my love for psychology after taking a few courses in the subjet. I knew that wherever I went to college, I would like to try to pursue a degree in Psychology.
+		<article>
+			<h2>Before College</h2>
+			<p> I moved to the States six years ago, moving to North Carolina. I went to a private high school called Salem Academy. The school was in a really small city called Winston-Salem, where Wake Forest University is, so I decided to go to a larger city where there are more resources and I can do more things. During high school, I found my love for psychology after taking a few courses in the subjet. I knew that wherever I went to college, I would like to try to pursue a degree in Psychology.</p>
+		</article>
+	</section>
 
-
-	</p>
-	
  	<h2>School/Internships</h2>
- 	<p> 
- 	My original plan was to double major in Psychology and International Relation, so I took many of these major courses during my freshman year. However, after a year, I came to realize that International Relation is not something that I am truly interested in. Then, I started looking into other majors, and eventually picked Digital Media Studies because it covers many different fields and is related to communication, which I am considering as my job path in the future.</p>
+ 	<p>My original plan was to double major in Psychology and International Relation, so I took many of these major courses during my freshman year. However, after a year, I came to realize that International Relation is not something that I am truly interested in. Then, I started looking into other majors, and eventually picked Digital Media Studies because it covers many different fields and is related to communication, which I am considering as my job path in the future.</p>
 
-	<p>Due to a special internship program in my high school, I’ve interned at two different companies during my high school time —- South China Morning Post and VMware. However, since I was still young and did not to too much in either of the field, my internship wasn’t as meaningful as my other experience later in college. Freshman year summer in college, I worked as an HR intern in IKEA, and I have loved the time working with the company and the co-workers. I like the working environment/company’s culture of IKEA; how everyone treated me like an actual employee to ask me do things; and how much I learned about work, new skills, and even new people that summer. 
-    </p>
+	<p>Due to a special internship program in my high school, I’ve interned at two different companies during my high school time —- South China Morning Post and VMware. However, since I was still young and did not to too much in either of the field, my internship wasn’t as meaningful as my other experience later in college. Freshman year summer in college, I worked as an HR intern in IKEA, and I have loved the time working with the company and the co-workers. I like the working environment/company’s culture of IKEA; how everyone treated me like an actual employee to ask me do things; and how much I learned about work, new skills, and even new people that summer.</p>
 
     <h2>Interests</h2>
     <p> 
